14cbe0b8da512525b374adc7a89946b21a789e19
Daniel7303 <danieleistien@gmail.com>
2025-10-22 23:13:54 +0100

    db

--- string_analyser_service/settings.py
+++ string_analyser_service/settings.py
@@ -67,9 +67,7 @@ WSGI_APPLICATION = 'string_analyser_service.wsgi.application'
 # Database
 # FIXED: Removed duplicate 'default' key
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
-    )
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=False)
 }
 
 # Password validation
